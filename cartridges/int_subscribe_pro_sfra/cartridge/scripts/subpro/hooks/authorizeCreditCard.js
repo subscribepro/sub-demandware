e2ffafea62575b665b495878097bd9212ccabd77
David King <imkingdavid@gmail.com>
2019-09-26 09:00:33 -0400

    Define what the payment authorization hook should return when payment fails.

--- cartridges/int_subscribe_pro_sfra/cartridge/scripts/subpro/hooks/authorizeCreditCard.js
+++ cartridges/int_subscribe_pro_sfra/cartridge/scripts/subpro/hooks/authorizeCreditCard.js
@@ -1,34 +1,30 @@
-var Logger = require('dw/system/Logger');
 var Status = require('dw/system/Status');
 var Transaction = require('dw/system/Transaction');
 var OrderMgr = require('dw/order/OrderMgr');
-// var PaymentMgr = require('dw/order/PaymentMgr');
-// var HookMgr = require('dw/system/HookMgr');
-// var COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
-// var hooksHelper = require('*/cartridge/scripts/helpers/hooks');
-// var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
 
 /*
  * authorizing credit card payment instrument
  * Hook: dw.ocapi.shop.order.authorizeCreditCard
  * */
 exports.authorizeCreditCard = function (order, paymentDetails, cvn) {
-
     // Attempt to process the transaction
 
-    // If it fails, return a Status with an ERROR type.
-    // The error code should be 'payment_error'
-    // The error message should be the error message from the gateway response
-    // A detail should be added to the status with the name being error_code from the gateway, and the value being the error message.
+    // If it fails, fail the order
     Transaction.wrap(function () {
-        OrderMgr.failOrder(order);
+        OrderMgr.failOrder(order, false);
     });
 
-    var status = new Status(Status.ERROR, 'error code', 'error message');
-    status.addDetail('detail1', { key: 'value' });
-    status.addDetail('detail2', 'string2');
-    status.addDetail('detail3', true);
-    status.addDetail('detaul4', 500);
+    // If it fails, return a Status with an ERROR type.
+
+    // The Status error code should be 'payment_error'
+    // The Status error message should be a generic message about failure to process the payment.
+    var status = new Status(Status.ERROR, 'payment_error', 'There was an error processing the payment.');
+
+    // Details should be added for the gateway_name, gateway_error_code, and gateway_error_message,
+    // which should be parsed from the gateway transaction response
+    status.addDetail('gateway_name', 'braintree');
+    status.addDetail('gateway_error_code', '2001');
+    status.addDetail('gateway_error_message', 'Insufficient Funds');
 
     return status;
 };
