3c69f488a8465f8d8bc1cfa3bd4da4a6b3246327
Alex Kozlov <o.kozlov@raccoon.pet>
2023-07-10 16:13:24 +0300

    sub-55: fix: Added synchrony SP registration for new SFCC customers

--- cartridges/int_subscribe_pro_sfra/cartridge/controllers/Account.js
+++ cartridges/int_subscribe_pro_sfra/cartridge/controllers/Account.js
@@ -4,23 +4,39 @@ var server = require('server');
 
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 var CustomerHelper = require('~/cartridge/scripts/subpro/helpers/customerHelper');
+var HookMgr = require('dw/system/HookMgr');
 
 server.extend(module.superModule);
 
-server.append('SaveProfile',
-    server.middleware.https,
-    csrfProtection.validateAjaxRequest,
-    function (req, res, next) {
-        if (customer.getProfile().custom.subproCustomerID) {
-            var profileForm = server.forms.getForm('profile');
-            CustomerHelper.updateCustomerInPlatform(customer.getProfile().custom.subproCustomerID, {
-                email: profileForm.customer.email.value,
-                first_name: profileForm.customer.firstname.value,
-                last_name: profileForm.customer.lastname.value,
-                platform_specific_customer_id: customer.getProfile().getCustomerNo()
-            });
+server.append('SaveProfile', server.middleware.https, csrfProtection.validateAjaxRequest, function (req, res, next) {
+    if (customer.getProfile().custom.subproCustomerID) {
+        var profileForm = server.forms.getForm('profile');
+        CustomerHelper.updateCustomerInPlatform(customer.getProfile().custom.subproCustomerID, {
+            email: profileForm.customer.email.value,
+            first_name: profileForm.customer.firstname.value,
+            last_name: profileForm.customer.lastname.value,
+            platform_specific_customer_id: customer.getProfile().getCustomerNo()
+        });
+    }
+    next();
+});
+
+server.append('Login', server.middleware.https, function (req, res, next) {
+    var customerSubproID = customer.getProfile().custom.subproCustomerID;
+    if (HookMgr.hasHook('app.registration.user.SPUser') && !customerSubproID) {
+        HookMgr.callHook('app.registration.user.SPUser', 'findOrCreateSPUser', customer);
+    }
+    next();
+});
+
+server.append('SubmitRegistration', server.middleware.https, csrfProtection.validateAjaxRequest, function (req, res, next) {
+    this.on('route:BeforeComplete', function (req, res) {
+        if (HookMgr.hasHook('app.registration.user.SPUser') && customer.getProfile()) {
+            HookMgr.callHook('app.registration.user.SPUser', 'findOrCreateSPUser', customer);
         }
-        next();
+        // eslint-disable-line no-shadow
     });
+    next();
+});
 
 module.exports = server.exports();
